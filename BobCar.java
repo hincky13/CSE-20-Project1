79acb610a91567d4902537638c319bb81af02fb0
pathey <32851560+pathey@users.noreply.github.com>
2017-10-16 13:12:29 -0700

    Add files via upload

---
+++ BobCar.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+//Written By:
+//Peter Athey
+//Justin Bollman
+
+public class BobCar {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Would you like:");
+		System.out.println("1. Economy: $35 per day");
+		System.out.println("2. Compact: $45 per day");
+		System.out.println("3. Standard: $95 per day");
+		
+		
+	}
+
+}
